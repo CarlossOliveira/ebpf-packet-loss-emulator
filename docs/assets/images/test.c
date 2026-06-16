465ff4ba80ef33bee9d583b334aa75aaf2912eda
Carlos Miguel Oliveira <carlosoliveira2006@icloud.com>
2026-06-16 19:09:58 +0100

    Documentation test

---
+++ docs/assets/images/test.c
@@ -0,0 +1,42 @@
+<a href="docs/user-guides/getting-started/setup.md">
+<svg width="144" height="120" viewBox="0 0 144 120"
+     xmlns="http://www.w3.org/2000/svg">
+
+    <style>
+        .border { stroke: currentColor; }
+        .next { fill: currentColor; opacity: 0.6; }
+        .setup { fill: #0078c8; }
+    </style>
+
+    <rect
+        class="border"
+        x="1"
+        y="1"
+        width="142"
+        height="118"
+        rx="18"
+        ry="18"
+        fill="none"
+        stroke-width="2"
+        opacity="0.3"
+    />
+
+    <text class="next"
+          x="109"
+          y="43"
+          text-anchor="middle"
+          font-size="24">
+        Next
+    </text>
+
+    <text class="setup"
+          x="72"
+          y="92"
+          text-anchor="middle"
+          font-size="28"
+          font-weight="700">
+        Setup
+    </text>
+
+</svg>
+</a>
